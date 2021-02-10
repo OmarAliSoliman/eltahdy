7952032aaa75b1e4daee87b88cf226a12a8a1d30
OmarAliSoliman <omarasmar9955@gmail.com>
2021-02-11 01:41:08 +0200

    add timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,37 @@ $(document).ready(function () {
 
   // $('select').select2();
 });
+
+
+dataTime = document.getElementById('timer').getAttribute('data-time');
+if (dataTime == 0) {
+  endTime();
+}
+document.getElementById('timer').innerHTML = dataTime + ":" + 00;
+startTimer();
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if (s == 59) { m = m - 1 }
+
+  //if(m<0){alert('timer completed')}
+
+  document.getElementById('timer').innerHTML =
+    m + ":" + s;
+  console.log(m)
+  setTimeout(startTimer, 1000);
+  if (s == 0 && m == 0) {
+    alert('done');
+    endTime();
+  }
+}
+function endTime() {
+  document.getElementById('timer-parent').innerHTML = "Done";
+}
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) { sec = "0" + sec }; // add zero in front of numbers < 10
+  if (sec < 0) { sec = "59" };
+  return sec;
+}

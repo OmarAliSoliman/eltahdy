65230f134722d192382f06d639806ac3140db257
OmarAliSoliman <omarasmar9955@gmail.com>
2021-03-22 16:16:04 +0200

    change slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
-  if ($(".footer-slider").length) {
-    $(".footer-slider").slick({
+  if ($(".footer-desktop-slider").length) {
+    $(".footer-desktop-slider").slick({
+      arrows: true,
+    });
+  }
+
+  if ($(".footer-mobile-slider").length) {
+    $(".footer-mobile-slider").slick({
       arrows: true,
     });
   }
@@ -15,35 +21,3 @@ $(document).ready(function () {
 });
 
 
-dataTime = document.getElementById('timer').getAttribute('data-time');
-if (dataTime == 0) {
-  endTime();
-}
-document.getElementById('timer').innerHTML = dataTime + ":" + 00;
-startTimer();
-function startTimer() {
-  var presentTime = document.getElementById('timer').innerHTML;
-  var timeArray = presentTime.split(/[:]+/);
-  var m = timeArray[0];
-  var s = checkSecond((timeArray[1] - 1));
-  if (s == 59) { m = m - 1 }
-
-  //if(m<0){alert('timer completed')}
-
-  document.getElementById('timer').innerHTML =
-    m + ":" + s;
-  console.log(m)
-  setTimeout(startTimer, 1000);
-  if (s == 0 && m == 0) {
-    alert('done');
-    endTime();
-  }
-}
-function endTime() {
-  document.getElementById('timer-parent').innerHTML = "Done";
-}
-function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) { sec = "0" + sec }; // add zero in front of numbers < 10
-  if (sec < 0) { sec = "59" };
-  return sec;
-}
